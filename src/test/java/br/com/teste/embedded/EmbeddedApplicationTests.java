f731937e761cbbded910bb65a00a3d8bcb59acf8
Gabriel Sene <sene300@gmail.com>
2023-03-27 19:18:30 -0300

    first commit

---
+++ src/test/java/br/com/teste/embedded/EmbeddedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.embedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
